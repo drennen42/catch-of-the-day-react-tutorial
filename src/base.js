80c27238b34f9c20bd085d0b3c9e5a7455b56152
Alex Drennen <alex.drennen@hyatt.com>
2017-01-18 01:25:29 -0600

    initializing catch of the day repo

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyDv6FRyxL_0ifSKGE5MaFgo1aspKPxCgi4",
+    authDomain: "catch-of-the-day-alex-drennen.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-alex-drennen.firebaseio.com"
+});
+
+export default base;
